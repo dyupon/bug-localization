aa5cf7e09d5e11daf77d9f2d940ec627455ff93c
Veronika Ponizova <ponizova.v@gmail.com>
2020-10-09 18:17:59 +0300

    added fetching line numbers of changes

--- bug_localization/get_changed_methods.py
+++ bug_localization/get_changed_methods.py
@@ -1,11 +1,26 @@
 import logging
 import re
+from collections import deque
 
 import git
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(levelname)s: %(message)s', level=logging.INFO)
 
 DIFF_LINES_PATTERN = "\\n@@\s-\d+(?:,\d+)?\s\+(\d+)(?:,)?((?:\d+)?)"
+
+
+def get_matching_bracket(s, i):
+    if s[i] != '{':
+        return -1
+    d = deque()
+    for k in range(i, len(s)):
+        if s[k] == '}':
+            d.popleft()
+        elif s[k] == '{':
+            d.append(s[i])
+        if not d:
+            return k
+    return -1
 
 
 def get_changed_lines(repo: git.repo.base.Repo, commit: git.objects.commit.Commit):
@@ -37,14 +52,53 @@ def get_changed_lines(repo: git.repo.base.Repo, commit: git.objects.commit.Commi
         return diff_lines
 
 
-def get_changed_methods(repo_path="../../intellij-community", commit_sha="a47e39be87fae50b80c2113858272af3ea9d62a5", ):
+def get_methods_borders(repo: git.repo.base.Repo, commit: git.objects.commit.Commit):
+    result = {}
+    for changed_file in commit.stats.files:
+        if not changed_file.endswith(".java") or changed_file.endswith(".kt"):
+            continue
+        file = repo.git.show('{}:{}'.format(commit.hexsha, changed_file))
+        s_to_linenumber = {}
+        num_of_lines = 0
+        num_of_symbols = 0
+        for s in range(len(file)):
+            if file[s] == '\n':
+                num_of_lines += 1
+            else:
+                s_to_linenumber[num_of_symbols] = num_of_lines
+                num_of_symbols += 1
+        if changed_file.endswith(".java"):
+            file = file.replace("\n", "")
+            main_obj_o = file.find("{")
+            main_obj_e = get_matching_bracket(file, main_obj_o)
+            inner_obj = file[main_obj_o + 1:main_obj_e]
+            inner_obj_list = list(inner_obj)
+            lambda_idx = inner_obj.find("-> {")
+            while lambda_idx > -1:
+                lambda_clos_idx = get_matching_bracket(inner_obj, lambda_idx + 3)
+                inner_obj_list[lambda_idx + 3] = "["
+                inner_obj_list[lambda_clos_idx] = "]"
+                inner_obj = "".join(inner_obj_list)
+                lambda_idx = inner_obj.find("-> {")
+            method_idx = inner_obj.find("{")
+            while method_idx > -1:
+                method_clos_idx = get_matching_bracket(inner_obj, method_idx + 3)
+                inner_obj_list[method_idx + 3] = "["
+                inner_obj_list[method_clos_idx] = "]"
+                start_line = s_to_linenumber.get(method_idx + main_obj_o)
+                # TODO: fetch method name from the start_line, append to the dict(result)
+                method_idx = inner_obj.find("{")
+        return result
+
+
+def get_changed_methods(repo_path="../../intellij-community",
+                        commit_sha="a47e39be87fae50b80c2113858272af3ea9d62a5"):
     repo = git.Repo(repo_path, odbt=git.db.GitDB)
     commit = repo.commit(commit_sha)
     diff_lines = get_changed_lines(repo, commit)
-    result = []
-    print(diff_lines)
-    return result
+    # method_lines = get_methods_borders(repo, commit)
+    return diff_lines
 
 
 if __name__ == '__main__':
-    get_changed_methods()
+    print(get_changed_methods())
