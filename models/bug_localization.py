ed86250288508851ca66dbc4c00505235c6111c6
dyupon <ponizova.v@gmail.com>
2020-12-14 15:59:58 +0300

    updated preprocessing

--- models/bug_localization.py
+++ models/bug_localization.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import GridSearchCV
 DIR_OUTPUT = "output/"
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment_name", type=str, default="all reports")
+    parser.add_argument("--experiment_name", type=str, default="all reports 3")
     parser.add_argument("--skip_reports_without_errors", type=str, default="no")
     config = parser.parse_args()
     DIR_OUTPUT += config.experiment_name
@@ -24,22 +24,35 @@ if __name__ == '__main__':
         shutil.rmtree(DIR_OUTPUT)
     os.makedirs(DIR_OUTPUT)
     print("Uploading data...")
-    df = pd.read_csv("data2.csv")
+    df = pd.read_csv("data3.csv")
     print("Preprocessing...")
-    df.drop("exception_type", axis=1, inplace=True)
     df.dropna(inplace=True)
     df = df.drop(df[(df.days_since_file_changed > -100500) & (df.days_since_file_changed <= -1)].index)
     df = df.replace({'days_since_file_changed': -100500}, -1)
-
     if config.skip_reports_without_errors == "yes":
         do_not_contain_errors = df.groupby("report_id").filter(lambda x: x["is_rootcause"].sum() == 0).index
         df = df.drop(do_not_contain_errors)
 
-    df.set_index("report_id", inplace=True)
+    num_cols = ["line_number", "distance_to_top", "frame_length", "days_since_file_changed",
+                "num_people_changed", "file_length", "method_length", "method_num_of_args", "file_num_lines"]
+    cat_cols = ["source"]
+    encode_cols = ["language", "exception_type"]
+    pk_cols = ["issue_id", "file_name", "frame"]
 
-    X = df[["timestamp", "line_number", "distance_to_top", "language",
-            "source", "frame_length", "days_since_file_changed", "num_people_changed", "file_length"]]
-    y = df[["is_rootcause"]]
+    df_onehot_dropfirst = df.copy()
+    for cat_col in encode_cols:
+        if len(df[cat_col].unique()) == 1:
+            encoded_col = pd.get_dummies(df[cat_col], prefix=cat_col)
+            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
+        else:
+            encoded_col = pd.get_dummies(df[cat_col], drop_first=True, prefix=cat_col)
+            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
+
+    df_onehot_dropfirst.set_index("report_id", inplace=True)
+
+    X = df_onehot_dropfirst.drop(pk_cols, axis=1)
+    y = df_onehot_dropfirst[["is_rootcause"]]
+    del df_onehot_dropfirst
     y.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
         DIR_OUTPUT + "/target_distribution.png")
     X_train, X_test, y_train, y_test = train_test_split(X, y,
@@ -82,15 +95,8 @@ if __name__ == '__main__':
     scaler = StandardScaler()
     X_train_std = X_train.copy()
     X_test_std = X_test.copy()
-    X_train_std[["line_number", "distance_to_top",
-                 "frame_length", "days_since_file_changed", "num_people_changed",
-                 "file_length"]] = scaler.fit_transform(X_train_std[[
-        "line_number", "distance_to_top", "frame_length", "days_since_file_changed", "num_people_changed",
-        "file_length"]])
-    X_test_std[["line_number", "distance_to_top",
-                "frame_length", "days_since_file_changed", "num_people_changed", "file_length"]] = scaler.transform(
-        X_test_std[["line_number", "distance_to_top", "frame_length", "days_since_file_changed", "num_people_changed",
-                    "file_length"]])
+    X_train_std[num_cols] = scaler.fit_transform(X_train_std[num_cols])
+    X_test_std[num_cols] = scaler.transform(X_test_std[num_cols])
 
     grid = {"C": [0.00000001, 0.0001, 0.001, 0.01, 0.1]}
     lr = LogisticRegression(class_weight="auto")
@@ -114,6 +120,34 @@ if __name__ == '__main__':
     RF OOB
     """
     print("RF OOB...")
+
+    del X_train
+    del y_train
+    del X_test
+    del y_test
+    del X
+    del y
+    del X_train_std
+    del X_test_std
+
+    df_onehot = df.copy()
+    for cat_col in encode_cols:
+        encoded_col = pd.get_dummies(df_onehot[cat_col], drop_first=True, prefix=cat_col)
+        df_onehot = pd.concat([df_onehot.drop(cat_col, axis=1), encoded_col], axis=1)
+    del df
+
+    df_onehot.set_index("report_id", inplace=True)
+    X = df_onehot.drop(pk_cols, axis=1)
+    y = df_onehot[["is_rootcause"]]
+    del df_onehot
+    y.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
+        DIR_OUTPUT + "/target_distribution.png")
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        axis="timestamp",
+                                                        test_size=0.25)
+    X_train.drop("timestamp", axis=1, inplace=True)
+    X_test.drop("timestamp", axis=1, inplace=True)
+
     clf = RandomForestClassifier(random_state=42, n_jobs=-1, oob_score=True)
     clf.fit(X_train, y_train.values.ravel())
     rf_predict = clf.predict(X_test)
