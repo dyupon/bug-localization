b66d07de9b285f4a1e6fc43919841686bc39b02a
dyupon <ponizova.v@gmail.com>
2020-12-21 01:52:43 +0300

    frame tokenizing

--- models/bug_localization.py
+++ models/bug_localization.py
@@ -3,6 +3,9 @@ import os
 import shutil
 import argparse
 import numpy as np
+import nltk
+from catboost import CatBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from models.metrics import report_accuracy
 from models.metrics import most_likely_error_accuracy
@@ -12,12 +15,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
+from datetime import datetime
 
 DIR_OUTPUT = "output/"
 if __name__ == '__main__':
+    startTime = datetime.now()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment_name", type=str, default="all reports 3")
-    parser.add_argument("--skip_reports_without_errors", type=str, default="no")
+    parser.add_argument("--experiment_name", type=str, default="skip error-free reports 4")
+    parser.add_argument("--skip_reports_without_errors", type=str, default="yes")
     config = parser.parse_args()
     DIR_OUTPUT += config.experiment_name
     if os.path.exists(DIR_OUTPUT):
@@ -37,7 +42,7 @@ if __name__ == '__main__':
                 "num_people_changed", "file_length", "method_length", "method_num_of_args", "file_num_lines"]
     cat_cols = ["source"]
     encode_cols = ["language", "exception_type"]
-    pk_cols = ["issue_id", "file_name", "frame"]
+    pk_cols = ["issue_id", "file_name"]
 
     df_onehot_dropfirst = df.copy()
     for cat_col in encode_cols:
@@ -53,6 +58,20 @@ if __name__ == '__main__':
     X = df_onehot_dropfirst.drop(pk_cols, axis=1)
     y = df_onehot_dropfirst[["is_rootcause"]]
     del df_onehot_dropfirst
+    del df
+    words = [x.split(".") for x in X['frame'].tolist()]
+    words = [item for sublist in words for item in sublist]
+    counts = pd.Series(nltk.ngrams(words, 3)).value_counts()
+    counts = counts[counts >= 1000]
+    vocab = [" ".join(x) for x in list(counts.index)]
+    X[["frame"]] = X["frame"].str.replace(".", " ")
+    vectorizer = CountVectorizer(vocabulary=vocab, ngram_range=(3, 3))
+    tokenized = vectorizer.fit_transform(X.frame.tolist())
+    X[vectorizer.get_feature_names()] = tokenized.toarray()
+    cat_cols = cat_cols + vectorizer.get_feature_names()
+    del tokenized
+    del vectorizer
+    X.drop("frame", axis=1, inplace=True)
     y.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
         DIR_OUTPUT + "/target_distribution.png")
     X_train, X_test, y_train, y_test = train_test_split(X, y,
@@ -89,65 +108,9 @@ if __name__ == '__main__':
         baseline.write(np.array2string(metrics.confusion_matrix(y_test, baseline_predict)))
 
     """
-    LR 
-    """
-    print("LR with GridSearchCV...")
-    scaler = StandardScaler()
-    X_train_std = X_train.copy()
-    X_test_std = X_test.copy()
-    X_train_std[num_cols] = scaler.fit_transform(X_train_std[num_cols])
-    X_test_std[num_cols] = scaler.transform(X_test_std[num_cols])
-
-    grid = {"C": [0.00000001, 0.0001, 0.001, 0.01, 0.1]}
-    lr = LogisticRegression(class_weight="auto")
-    logreg_cv = GridSearchCV(lr, grid, cv=5, scoring="f1")
-    logreg_cv.fit(X_train_std, y_train.values.ravel())
-    lr = logreg_cv.best_estimator_
-    lr.fit(X_train_std, y_train.values.ravel())
-    lr_predict = lr.predict(X_test_std)
-    lr_proba = lr.predict_proba(X_test_std)
-    with open(DIR_OUTPUT + "/results.txt", "a") as lf:
-        lf.write("\n \n------------- LR GridSearchCV ------------- \n")
-        lf.write("Best score: {} using {} \n".format(logreg_cv.best_score_, logreg_cv.best_params_))
-        lf.write("Mean of scores in CV: {} \n".format(logreg_cv.cv_results_['mean_test_score']))
-        lf.write("Std of scores in CV: {} \n".format(logreg_cv.cv_results_['std_test_score']))
-        lf.write("F1 score for LR Classifier: {:.3f} \n".format(metrics.f1_score(y_test, lr_predict)))
-        lf.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, lr_predict)))
-        lf.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, lr_proba)))
-        lf.write(np.array2string(metrics.confusion_matrix(y_test, lr_predict)))
-
-    """
     RF OOB
     """
     print("RF OOB...")
-
-    del X_train
-    del y_train
-    del X_test
-    del y_test
-    del X
-    del y
-    del X_train_std
-    del X_test_std
-
-    df_onehot = df.copy()
-    for cat_col in encode_cols:
-        encoded_col = pd.get_dummies(df_onehot[cat_col], drop_first=True, prefix=cat_col)
-        df_onehot = pd.concat([df_onehot.drop(cat_col, axis=1), encoded_col], axis=1)
-    del df
-
-    df_onehot.set_index("report_id", inplace=True)
-    X = df_onehot.drop(pk_cols, axis=1)
-    y = df_onehot[["is_rootcause"]]
-    del df_onehot
-    y.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
-        DIR_OUTPUT + "/target_distribution.png")
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        axis="timestamp",
-                                                        test_size=0.25)
-    X_train.drop("timestamp", axis=1, inplace=True)
-    X_test.drop("timestamp", axis=1, inplace=True)
-
     clf = RandomForestClassifier(random_state=42, n_jobs=-1, oob_score=True)
     clf.fit(X_train, y_train.values.ravel())
     rf_predict = clf.predict(X_test)
@@ -172,7 +135,7 @@ if __name__ == '__main__':
     print("RF with GridSearchCV...")
     clf = RandomForestClassifier(random_state=42, n_jobs=-1)
     param_grid = {
-        'n_estimators': [2, 3, 4, 5, 6, 7, 8, 9, 10, 15],
+        'n_estimators': [2, 4, 6, 8, 10, 15],
         'max_features': ['auto', 'sqrt', 'log2'],
         'min_samples_split': [2, 3, 4],
         'max_depth': [2, 3, 4, 5, 6, 7, 10, 15],
@@ -199,3 +162,60 @@ if __name__ == '__main__':
         rff.write(np.array2string(metrics.confusion_matrix(y_test, rf_predict)))
         rff.write("\n Feature importance \n")
         rff.write(feature_importance.to_string())
+
+    """
+    CatBoost
+    """
+    print("CatBoost CV...")
+    param_grid = {'depth': [3, 5, 7, 9, 11],
+                  'iterations': [10, 50, 100, 150, 300],
+                  'learning_rate': [0.03, 0.001, 0.01, 0.1, 0.2, 0.3],
+                  'l2_leaf_reg': [3, 1, 5, 10, 100],
+                  'border_count': [32, 5, 10, 20, 50, 100, 200]}
+
+    cb_model = CatBoostClassifier(custom_metric="F1", eval_metric="F1", random_seed=42)
+    cv_cb = GridSearchCV(estimator=cb_model, param_grid=param_grid, cv=5)
+    cv_cb.fit(X_train, y_train.values.ravel(), cat_features=cat_cols)
+    clf = cv_cb.best_estimator_
+    clf.fit(X_train, y_train.values.ravel())
+    cf_predict = clf.predict(X_test)
+    cf_proba = clf.predict_proba(X_test)
+    with open(DIR_OUTPUT + "/results.txt", "a") as cb:
+        cb.write("\n \n------------- CatBoost GridSearchCV ------------- \n")
+        cb.write("Best score: {} using {} \n".format(cv_cb.best_score_, cv_cb.best_params_))
+        cb.write("Mean of scores in CV: {} \n".format(cv_cb.cv_results_['mean_test_score']))
+        cb.write("Std of scores in CV: {} \n".format(cv_cb.cv_results_['std_test_score']))
+        cb.write("F1 score for RF Classifier: {:.3f} \n".format(metrics.f1_score(y_test, cf_predict)))
+        cb.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, cf_predict)))
+        cb.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, cf_proba)))
+        cb.write(np.array2string(metrics.confusion_matrix(y_test, cf_predict)))
+
+    """
+    LR 
+    """
+    print("LR with GridSearchCV...")
+    scaler = StandardScaler()
+    X_train_std = X_train
+    X_test_std = X_test
+    X_train_std[num_cols] = scaler.fit_transform(X_train_std[num_cols])
+    X_test_std[num_cols] = scaler.transform(X_test_std[num_cols])
+
+    grid = {"C": [0.00000001, 0.0001, 0.001, 0.01, 0.1]}
+    lr = LogisticRegression(class_weight="auto")
+    logreg_cv = GridSearchCV(lr, grid, cv=5, scoring="f1")
+    logreg_cv.fit(X_train_std, y_train.values.ravel())
+    lr = logreg_cv.best_estimator_
+    lr.fit(X_train_std, y_train.values.ravel())
+    lr_predict = lr.predict(X_test_std)
+    lr_proba = lr.predict_proba(X_test_std)
+    with open(DIR_OUTPUT + "/results.txt", "a") as lf:
+        lf.write("\n \n------------- LR GridSearchCV ------------- \n")
+        lf.write("Best score: {} using {} \n".format(logreg_cv.best_score_, logreg_cv.best_params_))
+        lf.write("Mean of scores in CV: {} \n".format(logreg_cv.cv_results_['mean_test_score']))
+        lf.write("Std of scores in CV: {} \n".format(logreg_cv.cv_results_['std_test_score']))
+        lf.write("F1 score for LR Classifier: {:.3f} \n".format(metrics.f1_score(y_test, lr_predict)))
+        lf.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, lr_predict)))
+        lf.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, lr_proba)))
+        lf.write(np.array2string(metrics.confusion_matrix(y_test, lr_predict)))
+
+    print(datetime.now() - startTime)
--- models/utils.py
+++ models/utils.py
@@ -6,20 +6,19 @@ import math
 
 def count_cv(estimator, df):
     metrics_list = []
-    for seed in [0, 17, 42]:
-        kf = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-        for train_index, test_index in kf.split(df.drop(["is_rootcause"], axis=1), df["is_rootcause"]):
-            fold_train_df = df.loc[train_index, :]
-            fold_test_df = df.loc[test_index, :]
-            estimator.fit(fold_train_df.drop(["is_rootcause"], axis=1).values, fold_train_df["is_rootcause"].values)
-            pred = estimator.predict(fold_test_df.drop(["is_rootcause"], axis=1).values)
-            f1 = metrics.f1_score(fold_test_df["is_rootcause"].values, pred)
-            metrics_list.append(f1)
+    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=62)
+    for train_index, test_index in kf.split(df.drop(["is_rootcause"], axis=1), df["is_rootcause"]):
+        fold_train_df = df.loc[train_index, :]
+        fold_test_df = df.loc[test_index, :]
+        estimator.fit(fold_train_df.drop(["is_rootcause"], axis=1).values, fold_train_df["is_rootcause"].values)
+        pred = estimator.predict(fold_test_df.drop(["is_rootcause"], axis=1).values)
+        f1 = metrics.f1_score(fold_test_df["is_rootcause"].values, pred)
+        metrics_list.append(f1)
     return np.round(np.mean(metrics_list), 4), np.round(np.std(metrics_list), 4)
 
 
-def train_test_split(X, y, axis, test_size):
-    df = X.copy()
+def train_test_split(df, y, axis, test_size):
+    # df = X.copy()
     df["is_rootcause"] = y
     df.sort_values(by=[axis], inplace=True)
     unique_idxs = list(df.index.unique())
