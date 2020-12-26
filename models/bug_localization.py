e6d93e6282f778255a9899a8f1d738267c7f017b
dyupon <ponizova.v@gmail.com>
2020-12-27 01:54:54 +0300

    hyperparameters tuning with hyperopt

--- models/bug_localization.py
+++ models/bug_localization.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import pandas as pd
 import os
 import shutil
@@ -7,6 +9,8 @@ import nltk
 from catboost import CatBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
+from sklearn.metrics import make_scorer, roc_auc_score
+from hyperopt import fmin, hp, tpe, Trials, space_eval
 from models.metrics import report_accuracy
 from models.metrics import most_likely_error_accuracy
 from models.baseline_classifiers import TopKClassifier
@@ -14,14 +18,30 @@ from models.utils import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 from datetime import datetime
+
+report_accuracy_scorer = make_scorer(report_accuracy)
+most_likely_error_scorer = make_scorer(most_likely_error_accuracy)
+
+
+def f_to_min(hps, X, y, model, scorer="roc_auc", ncv=5, fit_params=None, verbose=None):
+    if verbose:
+        model = model(**hps, verbose=verbose)
+    else:
+        model = model(**hps)
+    if fit_params:
+        cv_res = cross_val_score(model, X, y, cv=StratifiedKFold(ncv), scoring=scorer, fit_params=fit_params, n_jobs=-1)
+    else:
+        cv_res = cross_val_score(model, X, y, cv=StratifiedKFold(ncv), scoring=scorer, n_jobs=-1)
+    return -cv_res.mean()
+
 
 DIR_OUTPUT = "output/"
 if __name__ == '__main__':
     startTime = datetime.now()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment_name", type=str, default="skip error-free reports 4")
+    parser.add_argument("--experiment_name", type=str, default="skip error-free report 4")
     parser.add_argument("--skip_reports_without_errors", type=str, default="yes")
     config = parser.parse_args()
     DIR_OUTPUT += config.experiment_name
@@ -44,39 +64,29 @@ if __name__ == '__main__':
     encode_cols = ["language", "exception_type"]
     pk_cols = ["issue_id", "file_name"]
 
-    df_onehot_dropfirst = df.copy()
-    for cat_col in encode_cols:
-        if len(df[cat_col].unique()) == 1:
-            encoded_col = pd.get_dummies(df[cat_col], prefix=cat_col)
-            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
-        else:
-            encoded_col = pd.get_dummies(df[cat_col], drop_first=True, prefix=cat_col)
-            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
-
-    df_onehot_dropfirst.set_index("report_id", inplace=True)
-
-    X = df_onehot_dropfirst.drop(pk_cols, axis=1)
-    y = df_onehot_dropfirst[["is_rootcause"]]
-    del df_onehot_dropfirst
-    del df
-    words = [x.split(".") for x in X['frame'].tolist()]
+    words = [x.split(".") for x in df['frame'].tolist()]
     words = [item for sublist in words for item in sublist]
     counts = pd.Series(nltk.ngrams(words, 3)).value_counts()
     counts = counts[counts >= 1000]
     vocab = [" ".join(x) for x in list(counts.index)]
-    X[["frame"]] = X["frame"].str.replace(".", " ")
+    df[["frame"]] = df["frame"].str.replace(".", " ")
     vectorizer = CountVectorizer(vocabulary=vocab, ngram_range=(3, 3))
-    tokenized = vectorizer.fit_transform(X.frame.tolist())
-    X[vectorizer.get_feature_names()] = tokenized.toarray()
+    tokenized = vectorizer.fit_transform(df.frame.tolist())
+    df[vectorizer.get_feature_names()] = tokenized.toarray()
     cat_cols = cat_cols + vectorizer.get_feature_names()
     del tokenized
     del vectorizer
+    df.set_index("report_id", inplace=True)
+    X = df.drop(pk_cols, axis=1)
+    y = df[["is_rootcause"]]
     X.drop("frame", axis=1, inplace=True)
     y.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
         DIR_OUTPUT + "/target_distribution.png")
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         axis="timestamp",
                                                         test_size=0.25)
+    del X
+    del y
     X_train.drop("timestamp", axis=1, inplace=True)
     X_test.drop("timestamp", axis=1, inplace=True)
     y_train.groupby("report_id").sum().plot.hist(bins=[0, 1, 2, 3, 4, 5, 10]).figure.savefig(
@@ -108,6 +118,72 @@ if __name__ == '__main__':
         baseline.write(np.array2string(metrics.confusion_matrix(y_test, baseline_predict)))
 
     """
+    CatBoost OOB
+    """
+    print("CatBoost OOB...")
+    clf = CatBoostClassifier(custom_metric="F1", eval_metric="F1", random_seed=42, thread_count=4, verbose=False)
+    clf.fit(X_train, y_train.values.ravel(), cat_features=cat_cols + encode_cols)
+    cf_predict = clf.predict(X_test)
+    cf_proba = clf.predict_proba(X_test)
+    with open(DIR_OUTPUT + "/results.txt", "a") as cb:
+        cb.write("\n \n------------- CatBoost OOB ------------- \n")
+        cb.write("ROC-AUC score: {} using {} \n".format(roc_auc_score(y_test, cf_proba[:, 1]), clf.get_all_params()))
+        cb.write("F1 score for CatBoostClassifier: {:.3f} \n".format(metrics.f1_score(y_test, cf_predict)))
+        cb.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, cf_predict)))
+        cb.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, cf_proba)))
+        cb.write(np.array2string(metrics.confusion_matrix(y_test, cf_predict)))
+
+    """
+    CatBoost hyperopt
+    """
+    print("CatBoost hyperopt...")
+    space4cb = {
+        'depth': hp.choice('depth', range(1, 15)),
+        'learning_rate': hp.uniform('learning_rate', 0.001, 0.3),
+        'l2_leaf_reg': hp.choice('l2_leaf_reg', range(1, 10)),
+        'border_count': hp.choice('border_count', range(1, 5)),
+        'iterations': hp.choice('iterations', [100, 150, 300, 500])
+    }
+    trials_cb = Trials()
+    best_clf = fmin(partial(f_to_min, X=X_train, y=y_train.values.ravel(), model=CatBoostClassifier,
+                            fit_params={"cat_features": cat_cols + encode_cols}, verbose=False),
+                    space4cb, algo=tpe.suggest, max_evals=100,
+                    trials=trials_cb, rstate=np.random.RandomState(42))
+    clf = CatBoostClassifier(**space_eval(space4cb, best_clf))
+    clf.fit(X_train, y_train.values.ravel(), cat_features=cat_cols + encode_cols)
+    cf_proba = clf.predict_proba(X_test)
+    clf_val_score = roc_auc_score(y_test, cf_proba[:, 1])
+    cf_predict = clf.predict(X_test)
+    with open(DIR_OUTPUT + "/results.txt", "a") as cb:
+        cb.write("\n \n------------- CatBoost hyperopt ------------- \n")
+        cb.write("Cross-val score: {} \n".format(-trials_cb.best_trial['result']['loss']))
+        cb.write("ROC-AUC score: {} using {} \n".format(clf_val_score, space_eval(space4cb, best_clf)))
+        cb.write("F1 score for RF Classifier: {:.3f} \n".format(metrics.f1_score(y_test, cf_predict)))
+        cb.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, cf_predict)))
+        cb.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, cf_proba)))
+        cb.write(np.array2string(metrics.confusion_matrix(y_test, cf_predict)))
+        cb.write("\n Feature importance \n")
+
+    df_onehot_dropfirst = df
+    for cat_col in encode_cols:
+        if len(df[cat_col].unique()) == 1:
+            encoded_col = pd.get_dummies(df[cat_col], prefix=cat_col)
+            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
+        else:
+            encoded_col = pd.get_dummies(df[cat_col], drop_first=True, prefix=cat_col)
+            df_onehot_dropfirst = pd.concat([df_onehot_dropfirst.drop(cat_col, axis=1), encoded_col], axis=1)
+    X = df_onehot_dropfirst.drop(pk_cols, axis=1)
+    y = df_onehot_dropfirst[["is_rootcause"]]
+    X.drop("frame", axis=1, inplace=True)
+    del df_onehot_dropfirst
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        axis="timestamp",
+                                                        test_size=0.25)
+    del X
+    del y
+    X_train.drop("timestamp", axis=1, inplace=True)
+    X_test.drop("timestamp", axis=1, inplace=True)
+    """
     RF OOB
     """
     print("RF OOB...")
@@ -132,63 +208,38 @@ if __name__ == '__main__':
     """
     RF tuned
     """
-    print("RF with GridSearchCV...")
-    clf = RandomForestClassifier(random_state=42, n_jobs=-1)
-    param_grid = {
-        'n_estimators': [2, 4, 6, 8, 10, 15],
-        'max_features': ['auto', 'sqrt', 'log2'],
-        'min_samples_split': [2, 3, 4],
-        'max_depth': [2, 3, 4, 5, 6, 7, 10, 15],
-        'criterion': ['gini', 'entropy']
+    print("RF with hyperopt...")
+    space4rf = {
+        'max_depth': hp.choice('max_depth', range(1, 20)),
+        'max_features': hp.choice('max_features', ['sqrt', 'log2']),
+        'n_estimators': hp.choice('n_estimators', range(1, 20)),
+        'criterion': hp.choice('criterion', ['gini', 'entropy']),
+        'class_weight': hp.choice('class_weight', [None, 'balanced'])
     }
-    cv_rf = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, scoring="f1")
-    cv_rf.fit(X_train, y_train.values.ravel())
-    clf = cv_rf.best_estimator_
+
+    trials_clf = Trials()
+    best_clf = fmin(partial(f_to_min, X=X_train, y=y_train.values.ravel(), model=RandomForestClassifier),
+                    space4rf, algo=tpe.suggest, max_evals=100,
+                    trials=trials_clf, rstate=np.random.RandomState(42))
+    clf = RandomForestClassifier(**space_eval(space4rf, best_clf))
     clf.fit(X_train, y_train.values.ravel())
-    rf_predict = clf.predict(X_test)
     rf_proba = clf.predict_proba(X_test)
+    clf_val_score = roc_auc_score(y_test, rf_proba[:, 1])
+    rf_predict = clf.predict(X_test)
     feature_importance = pd.DataFrame(clf.feature_importances_,
                                       index=X_train.columns,
                                       columns=['importance']).sort_values('importance', ascending=False)
 
     with open(DIR_OUTPUT + "/results.txt", "a") as rff:
         rff.write("\n \n------------- RF GridSearchCV ------------- \n")
-        rff.write("Best score: {} using {} \n".format(cv_rf.best_score_, cv_rf.best_params_))
-        rff.write("Mean of scores in CV: {} \n".format(cv_rf.cv_results_['mean_test_score']))
-        rff.write("Std of scores in CV: {} \n".format(cv_rf.cv_results_['std_test_score']))
+        rff.write("Cross-val score: {} \n".format(-trials_clf.best_trial['result']['loss']))
+        rff.write("ROC-AUC score: {} using {} \n".format(clf_val_score, space_eval(space4rf, best_clf)))
         rff.write("F1 score for RF Classifier: {:.3f} \n".format(metrics.f1_score(y_test, rf_predict)))
         rff.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, rf_predict)))
         rff.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, rf_proba)))
         rff.write(np.array2string(metrics.confusion_matrix(y_test, rf_predict)))
         rff.write("\n Feature importance \n")
         rff.write(feature_importance.to_string())
-
-    """
-    CatBoost
-    """
-    print("CatBoost CV...")
-    param_grid = {'depth': [3, 5, 7, 9, 11],
-                  'iterations': [10, 50, 100, 150, 300],
-                  'learning_rate': [0.03, 0.001, 0.01, 0.1, 0.2, 0.3],
-                  'l2_leaf_reg': [3, 1, 5, 10, 100],
-                  'border_count': [32, 5, 10, 20, 50, 100, 200]}
-
-    cb_model = CatBoostClassifier(custom_metric="F1", eval_metric="F1", random_seed=42)
-    cv_cb = GridSearchCV(estimator=cb_model, param_grid=param_grid, cv=5)
-    cv_cb.fit(X_train, y_train.values.ravel(), cat_features=cat_cols)
-    clf = cv_cb.best_estimator_
-    clf.fit(X_train, y_train.values.ravel())
-    cf_predict = clf.predict(X_test)
-    cf_proba = clf.predict_proba(X_test)
-    with open(DIR_OUTPUT + "/results.txt", "a") as cb:
-        cb.write("\n \n------------- CatBoost GridSearchCV ------------- \n")
-        cb.write("Best score: {} using {} \n".format(cv_cb.best_score_, cv_cb.best_params_))
-        cb.write("Mean of scores in CV: {} \n".format(cv_cb.cv_results_['mean_test_score']))
-        cb.write("Std of scores in CV: {} \n".format(cv_cb.cv_results_['std_test_score']))
-        cb.write("F1 score for RF Classifier: {:.3f} \n".format(metrics.f1_score(y_test, cf_predict)))
-        cb.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, cf_predict)))
-        cb.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, cf_proba)))
-        cb.write(np.array2string(metrics.confusion_matrix(y_test, cf_predict)))
 
     """
     LR 
@@ -200,19 +251,25 @@ if __name__ == '__main__':
     X_train_std[num_cols] = scaler.fit_transform(X_train_std[num_cols])
     X_test_std[num_cols] = scaler.transform(X_test_std[num_cols])
 
-    grid = {"C": [0.00000001, 0.0001, 0.001, 0.01, 0.1]}
-    lr = LogisticRegression(class_weight="auto")
-    logreg_cv = GridSearchCV(lr, grid, cv=5, scoring="f1")
-    logreg_cv.fit(X_train_std, y_train.values.ravel())
-    lr = logreg_cv.best_estimator_
-    lr.fit(X_train_std, y_train.values.ravel())
-    lr_predict = lr.predict(X_test_std)
-    lr_proba = lr.predict_proba(X_test_std)
+    space4lr = {
+        'C': hp.loguniform('logit.C', -4.0 * np.log(10.0), 4.0 * np.log(10.0)),
+        'class_weight': hp.choice('logit.class_weight', [None, 'balanced'])
+    }
+
+    trials_clf = Trials()
+    best_clf = fmin(partial(f_to_min, X=X_train_std, y=y_train.values.ravel(), model=LogisticRegression),
+                    space4lr, algo=tpe.suggest, max_evals=100,
+                    trials=trials_clf, rstate=np.random.RandomState(42))
+
+    clf = LogisticRegression(**space_eval(space4lr, best_clf))
+    clf.fit(X_train_std, y_train.values.ravel())
+    lr_proba = clf.predict_proba(X_test_std)
+    clf_val_score = roc_auc_score(y_test, lr_proba[:, 1])
+    lr_predict = clf.predict(X_test_std)
     with open(DIR_OUTPUT + "/results.txt", "a") as lf:
-        lf.write("\n \n------------- LR GridSearchCV ------------- \n")
-        lf.write("Best score: {} using {} \n".format(logreg_cv.best_score_, logreg_cv.best_params_))
-        lf.write("Mean of scores in CV: {} \n".format(logreg_cv.cv_results_['mean_test_score']))
-        lf.write("Std of scores in CV: {} \n".format(logreg_cv.cv_results_['std_test_score']))
+        lf.write("\n \n------------- LR hyperopt ------------- \n")
+        lf.write("Cross-val score: {} \n".format(-trials_clf.best_trial['result']['loss']))
+        lf.write("ROC-AUC score: {} using {} \n".format(clf_val_score, space_eval(space4lr, best_clf)))
         lf.write("F1 score for LR Classifier: {:.3f} \n".format(metrics.f1_score(y_test, lr_predict)))
         lf.write("Accuracy for reports: {:.3f} \n".format(report_accuracy(y_test, lr_predict)))
         lf.write("Accuracy for most likely rootcauses: {:.3f} \n".format(most_likely_error_accuracy(y_test, lr_proba)))
